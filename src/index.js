8e6c97bdf2a93ee9c761008f84c093a908acbb55
Chyld Medford <chyld.medford@gmail.com>
2016-03-18 20:33:42 +0000

    changed function names

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
 import Request from './request';
 
 class ObjectStorage {
-  constructor(userId, password, projectId, accessPoint = 'https://dal.objectstorage.open.softlayer.com'){
+  constructor(userId, password, projectId, container, accessPoint = 'https://dal.objectstorage.open.softlayer.com'){
     this.credentials = require('../config/credentials.json');
     this.userId = userId;
     this.password = password;
     this.projectId = projectId;
+    this.container = container;
     this.endpoint = `${accessPoint}/v1/AUTH_${projectId}`;
     this.token = null;
+    this.url = `${this.endpoint}/${this.container}`;
   }
-  create(container){
-    const url = `${this.endpoint}/${container}`;
+  createContainer(){
+    const url = this.url;
     const method = 'put';
     const headers = {'x-auth-token': this.token};
-    return Request({url, method, headers, json: true}, this);
+    return Request({url, method, headers, json: true}, this).then(() => null);
   }
-  list(container){
-    const url = `${this.endpoint}/${container}`;
+  listContainerFiles(){
+    const url = this.url;
     const method = 'get';
     const headers = {'x-auth-token': this.token};
-    return Request({url, method, headers, json: true}, this);
+    return Request({url, method, headers, json: true}, this).then(({response, body}) => body.map(file => `${this.url}/${file.name}`));
   }
-  unlock(container){
-    const url = `${this.endpoint}/${container}`;
+  setContainerPublicReadable(){
+    const url = this.url;
     const method = 'post';
     const headers = {'x-auth-token': this.token, 'x-container-read': '.r:*, .rlistings'};
-    return Request({url, method, headers, json: true}, this);
+    return Request({url, method, headers, json: true}, this).then(() => null);
   }
-  upload(container, filename, mimetype, buffer, size){
-    const url = `${this.endpoint}/${container}/${filename}`;
+  uploadFileToContainer(filename, mimetype, buffer, size){
+    const url = `${this.url}/${filename}`;
     const method = 'put';
     const headers = {'x-auth-token': this.token, 'content_type': mimetype, 'content-length': size};
-    return Request({url, method, headers, body: buffer}, this);
+    return Request({url, method, headers, body: buffer}, this).then(() => `${this.url}/${filename}`);
   }
 }
 
--- test/index.js
+++ test/index.js
@@ -7,7 +7,7 @@ describe('ObjectStorage', function(){
   var os;
 
   beforeEach(function(){
-      os = new ObjectStorage('a', 'b', 'c');
+      os = new ObjectStorage('userid', 'password', 'projectid', 'container');
   });
 
   afterEach(function(){
@@ -16,51 +16,51 @@ describe('ObjectStorage', function(){
 
   describe('constructor', function(){
     it('should create an object of type ObjectStorage', function(){
-      var os1 = new ObjectStorage('d', 'e', 'f');
+      var os1 = new ObjectStorage('d', 'e', 'f', 'g');
       expect(os1.userId).to.equal('d');
       expect(os1.password).to.equal('e');
       expect(os1.projectId).to.equal('f');
+      expect(os1.container).to.equal('g');
       expect(os1.endpoint).to.equal('https://dal.objectstorage.open.softlayer.com/v1/AUTH_f');
     });
   });
 
-  describe('#create', function(){
+  describe('#createContainer', function(){
     it('should create a container', function(done){
-      nock('https://dal.objectstorage.open.softlayer.com').put('/v1/AUTH_c/bucket').reply(201);
-      os.create('bucket').then(function(data){
-        expect(data.response.statusCode).to.equal(201);
+      nock('https://dal.objectstorage.open.softlayer.com').put('/v1/AUTH_projectid/container').reply(201);
+      os.createContainer().then(function(data){
+        expect(data).to.be.null;
         done();
       });
     });
   });
 
-    describe('#list', function(){
+    describe('#listContainerFiles', function(){
     it('should return a list of files in a container', function(done){
-      nock('https://dal.objectstorage.open.softlayer.com').get('/v1/AUTH_c/bucket').reply(200, [3, 5, 7]);
-      os.list('bucket').then(function(data){
-        expect(data.response.statusCode).to.equal(200);
-        expect(data.body).to.have.length(3);
+      nock('https://dal.objectstorage.open.softlayer.com').get('/v1/AUTH_projectid/container').reply(200, [3, 5, 7]);
+      os.listContainerFiles().then(function(data){
+        expect(data).to.have.length(3);
         done();
       });
     });
   });
 
-    describe('#unlock', function(){
+    describe('#setContainerPublicReadable', function(){
     it('should make a container read only to the public', function(done){
-      nock('https://dal.objectstorage.open.softlayer.com').post('/v1/AUTH_c/bucket').reply(204);
-      os.unlock('bucket').then(function(data){
-        expect(data.response.statusCode).to.equal(204);
+      nock('https://dal.objectstorage.open.softlayer.com').post('/v1/AUTH_projectid/container').reply(204);
+      os.setContainerPublicReadable().then(function(data){
+        expect(data).to.be.null;
         done();
       });
     });
   });
 
-    describe('#upload', function(){
+  describe('#uploadFileToContainer', function(){
     it('should upload a file to the container', function(done){
       var buffer = fs.readFileSync('./test/fixtures/pup.jpg');
-      nock('https://dal.objectstorage.open.softlayer.com').put('/v1/AUTH_c/bucket/pup.jpg').reply(201);
-      os.upload('bucket', 'pup.jpg', 'image/jpeg', buffer, 32796).then(function(data){
-        expect(data.response.statusCode).to.equal(201);
+      nock('https://dal.objectstorage.open.softlayer.com').put('/v1/AUTH_projectid/container/pup.jpg').reply(201);
+      os.uploadFileToContainer('pup.jpg', 'image/jpeg', buffer, 32796).then(function(data){
+        expect(data).to.equal('https://dal.objectstorage.open.softlayer.com/v1/AUTH_projectid/container/pup.jpg');
         done();
       });
     });
